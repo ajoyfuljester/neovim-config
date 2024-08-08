69586ebf1f5e9c1fc591d07d9f79441501bcf9e4
ajoyfuljester <ajoyfuljester@protonmail.com>
2024-08-08 15:01:43 +0200

    change telescope shortcut because i didn't test it

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>e', builtin.find_files, {})
+vim.keymap.set('n', '<leader>w', builtin.find_files, {})
 vim.keymap.set('n', '<A-e>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")})
