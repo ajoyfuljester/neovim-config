c70ed2f88e2bbe1203e7283402373591e4bdc27d
ajoyfuljester <ajoyfuljester@protonmail.com>
2024-08-22 21:06:53 +0200

    fix emmet

--- lua/ajoyfuljester/init.lua
+++ lua/ajoyfuljester/init.lua
@@ -1,5 +1,7 @@
-vim.g.user_emmet_leader_key = '<c-z>'
-
+vim.g.user_emmet_leader_key = '<c-s>'
+vim.g.user_emmet_expandabbr_key = '<c-s>'
+vim.g.user_emmet_install_global = 0
+vim.cmd('autocmd FileType html,css EmmetInstall')
 require("ajoyfuljester.remap")
 require("ajoyfuljester.lazy")
 require("ajoyfuljester.set")
