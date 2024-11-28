6be1ff9cb04448a3a53a630e7d01f26252e0bad2
ajoyfuljester <ajoyfuljester@protonmail.com>
2024-11-28 16:33:44 +0100

    change keymap because it was inverted

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,18 @@
+local telescope = require('telescope')
+
+telescope.setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<C-p>"] = "move_selection_next",
+				["<C-n>"] = "move_selection_previous",
+			}
+		}
+	}
+})
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>w', builtin.find_files, {})
-vim.keymap.set('n', '<A-e>', builtin.git_files, {})
+vim.keymap.set('n', '<A-e>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ")})
 end)
